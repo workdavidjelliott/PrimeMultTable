730ae2e1194e48d4204ebaab140ffd9aafe27310
David Elliott <dave.elliott@codevelop.co.uk>
2015-11-03 19:10:26 +0000

    Initial Commit

---
+++ PrimeMultTable/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PrimeMultTable
+//
+//  Created by Dave Elliott on 03/11/2015.
+//  Copyright © 2015 David Elliott. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
