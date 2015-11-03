a7350428e18e13f0e6d6c3ffe218d7c86b58c5d6
David Elliott <dave.elliott@codevelop.co.uk>
2015-11-03 21:17:56 +0000

    Added a regression test for the bug with the irregular sized table.

--- PrimeMultTableTests/IntTableTest.swift
+++ PrimeMultTableTests/IntTableTest.swift
@@ -54,4 +54,27 @@ class IntTableTest: XCTestCase {
         }
         
     }
+    func testPreCalculatedIrregularTable()
+    {
+        let table:IntTable = IntTable(algorithm:TableAlgorithmMultiply(),topRow: [1,2,3,4,5,6,7,8,9,10], leftColumn: [5,4,3,2,1])
+        
+        var resultTest:[Int?] =
+        [
+            nil,1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+             5, 5,10,15,20,25,30,35,40,45,50,
+             4, 4, 8,12,16,20,24,28,32,36,40,
+             3, 3, 6, 9,12,15,18,21,24,27,30,
+             2, 2, 4, 6, 8,10,12,14,16,18,20,
+             1, 1, 2, 3, 4, 5, 6, 7, 8,9,10
+        ]
+        
+        for y in 0..<5
+        {
+            for x in 0..<5
+            {
+                XCTAssert(resultTest[(y*11) + x] == table.getAtPosition(x,row:y))
+            }
+        }
+        
+    }
 }
