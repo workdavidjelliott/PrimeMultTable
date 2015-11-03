128b18447b937b6b14b477db9ab7eded9ee513bd
David Elliott <dave.elliott@codevelop.co.uk>
2015-11-03 20:34:05 +0000

    First attempt at implementation. Failed on array out of bounds test.

--- PrimeMultTable/IntTable.swift
+++ PrimeMultTable/IntTable.swift
@@ -10,14 +10,33 @@ import Foundation
 
 class IntTable
 {
+    // TODO : Update this so these arrays can dynamically change.
+    // No test coverage yet.
+    private var topRow:[Int]
+    private var leftColumn:[Int]
+    
     init(topRow:[Int], leftColumn:[Int])
     {
-        
+        self.topRow = topRow
+        self.leftColumn = leftColumn
     }
     
     func getAtPosition(column:Int, row:Int) -> Int?
     {
-        return nil
+        if row == 0 && column == 0
+        {
+            return nil
+        }
+        if row == 0
+        {
+            return topRow[column-1]
+        }
+        if column == 0
+        {
+            return leftColumn[row-1]
+        }
+        return topRow[column-1] * leftColumn[row-1]
     }
     
+
 }
