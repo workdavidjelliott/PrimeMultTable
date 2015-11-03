9151a1e9c7b6db328030fe38b0a3f20671dd27c2
David Elliott <dave.elliott@codevelop.co.uk>
2015-11-03 19:41:30 +0000

    Created a very simple prime function without any optimisation. Passed all tests.

--- PrimeMultTable/Prime.swift
+++ PrimeMultTable/Prime.swift
@@ -12,6 +12,21 @@ extension Int
 {
     func isPrime() -> Bool
     {
+        // No primes under 2
+        if self < 2
+        {
+            return false
+        }
+
+        // For all numbers up to the prime candidate
+        for i in 2..<self
+        {
+            // Check if the prime candidate is divisible.
+            if (self % i) == 0
+            {
+                return false
+            }
+        }
         return true
     }
 }
